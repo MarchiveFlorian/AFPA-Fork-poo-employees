3428131e15c072fab4224fdbb51f1ba8a2791e0e
Florian Marchive <marchiveflorian@gmail.com>
2024-06-20 12:33:49 +0200

    exo terminé

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -9,31 +9,75 @@ Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
 
-class Employee
-{
+class Employee {
 	/**
 	 * Matricule de l'employé
 	 */
 	private String registrationNumber;
 
 	// TODO compléter les attributs comme présenté dans le PDF
-
+	
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private final int socialRate = 30;
+	
 	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	public Employee(String registrationNumber, String lastName, String firstName, double salary) {
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
 	}
-
-	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+	
+	// TODO implémenter les setters et getters de la classe (permet d'accéder aux
+	// attributs privés)
+	
+	// Getters
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
-
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public int getSocialRate() {
+		return socialRate;
+	}
+	
+	// Setters
 	public void setRegistrationNumber(String registrationNumber) throws Exception {
 		this.registrationNumber = registrationNumber;
 	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 
-	// TODO ajouter les getters et les setters
-
-	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
-	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère
+	// qui représente un objet de la classe employé
+	// plus d'information sur la méthode "toString()" ->
+	// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	@Override
+	public String toString() {
+		return "Employee [registrationNumber=" + registrationNumber + ", lastName=" + lastName + ", firstName="
+				+ firstName + ", salary=" + salary + ", socialRate=" + socialRate + "]";
+	}
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -13,9 +13,16 @@ class EmployeeMain
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
 		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
-
+		Employee employee1 = new Employee("11ABC22", "Dupont", "Martin", 2500);
+		Employee employee2 = new Employee("22ABC33", "Eboue", "Fabrice", 10000);
+		Employee employee3 = new Employee("33ABC44", "Dujardin", "Jean", 15000);
+		Employee employee4 = new Employee("44ABC55", "Random", "Guy", 9999);
+		
 		// TODO afficher les informations des employés avec System.out.println
+		System.out.println(employee1.toString());
+		System.out.println(employee2.toString());
+		System.out.println(employee3.toString());
+		System.out.println(employee4.toString());
 	}
 }
 
